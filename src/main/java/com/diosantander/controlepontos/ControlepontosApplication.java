588aa5a040a85ebe3e586832399f5a695b46995f
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-20 14:13:09 -0300

    Create Project

---
+++ src/main/java/com/diosantander/controlepontos/ControlepontosApplication.java
@@ -0,0 +1,13 @@
+package com.diosantander.controlepontos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlepontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlepontosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diosantander/controlepontos/ControlepontosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diosantander.controlepontos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlepontosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
