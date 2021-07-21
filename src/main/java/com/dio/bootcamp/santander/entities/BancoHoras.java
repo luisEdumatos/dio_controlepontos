287593cd6161b9ce840f56f7bab2c4b146d1907f
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-21 08:36:55 -0300

    Create Entities Movimentacao e BancoHoras

--- src/main/java/com/dio/bootcamp/santander/entities/BancoHoras.java
+++ src/main/java/com/dio/bootcamp/santander/entities/BancoHoras.java
@@ -2,6 +2,10 @@ package com.dio.bootcamp.santander.entities;
 
 import lombok.*;
 
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -10,8 +14,22 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class BancoHoras {
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Data
+    @Embeddable
+    public  class BancoHorasId implements Serializable {
+        private long idBancoHoras;
+        private long idMovimentacao;
+        private long idUsuario;
+    }
+
+    @EmbeddedId
+    private BancoHorasId bancoHorasId;
     private LocalDateTime dataTrabalhada;
     private BigDecimal quantidadeHoras;
     private BigDecimal saldoHoras;
--- src/main/java/com/dio/bootcamp/santander/entities/Movimentacao.java
+++ src/main/java/com/dio/bootcamp/santander/entities/Movimentacao.java
@@ -2,6 +2,8 @@ package com.dio.bootcamp.santander.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -10,9 +12,26 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class Movimentacao {
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class MovimentacaoId implements Serializable {
+        private long idMovimento;
+        private long idUsuario;
+    }
+
+    @Id
+    @EmbeddedId
+    private MovimentacaoId movimentacaoId;
     private LocalDateTime dataEntrada;
     private LocalDateTime dataSaida;
     private BigDecimal periodo;
+    @ManyToOne
+    private Ocorrencia ocorrencia;
+    @ManyToOne
+    private Calendario calendario;
 }
